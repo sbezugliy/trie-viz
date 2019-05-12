c64f4df6ef2e00472d17ce5b6ed446b871fa8857
Sergey Bezugliy <s.bezugliy@gmail.com>
2019-05-12 12:16:46 +0300

    Changed separator from space to semicolon. etc...

--- lib/trie/application/web.rb
+++ lib/trie/application/web.rb
@@ -5,10 +5,13 @@ require 'sinatra/reloader' if development?
 require 'tss/tss'
 require 'trie/viz'
 require 'haml'
+require 'benchmark'
 
 ##
 # Web application starter class
 class Web < Sinatra::Base
+  enable :sessions
+
   configure :development do
     register Sinatra::Reloader
   end
@@ -17,6 +20,14 @@ class Web < Sinatra::Base
 
   set :views, File.expand_path('views', __dir__)
   set :haml, format: :html5
+
+  set :results, {}
+
+  set :flash, {}
+
+  after do
+    settings.flash = {}
+  end
 
   get '/' do
     haml :'/index'
@@ -30,28 +41,42 @@ class Web < Sinatra::Base
     image_name = 'trie.png'
     diagram_path = File.join(settings.public_folder, 'images', image_name)
     dictionary = dict_to_arr(params[:dictionary])
-    pp dictionary
-    tss = TSS::Trie.new(dictionary, :full)
+    @tss = nil
+    @inclusions = nil
+    bm_build = Benchmark.measure do
+      @tss = TSS::Trie.new(dictionary, :full)
+    end
+    bm_parse = Benchmark.measure do
+      @inclusions = @tss.parse(params[:text]) if params[:text]
+    end
+    TrieViz::Diagram.new(@tss.root, diagram_path)
 
-    inclusions = tss.parse(params[:text]) if params[:text]
-
-    TrieViz::Diagram.new(tss.root, diagram_path)
-
-    haml :'/result', locals: {
+    @results = {
       data: {
         dictionary: dictionary,
         text: params[:text]
       },
       result: {
         diagram_path: 'images/' + image_name,
-        inclusions: inclusions
+        inclusions: @inclusions
+      },
+      benchmark: {
+        build: bm_build,
+        parse: bm_parse
       }
+    }
+    haml :'/result', locals: @results
+  rescue RuntimeError => e
+    pp e.message
+    settings.flash[:message] = "#{e.class}. Please check fields"
+    haml :'/dictionary', locals: {
+      results: @results
     }
   end
 
   private
 
   def dict_to_arr(dict)
-    dict.scan(/(?:[^\\\s]|\\.)+/).map{ |s| s.gsub('\ ', ' ') }
+    dict.split(/(?<!\\); /).map{|s| s.gsub('\\;', ';')}
   end
 end
